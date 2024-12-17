d4b422bdcc83c0a8fe468dd1e9a30cdde458f56a
Arisa Shiraishi <arisashiraishi@Arisas-MacBook-Pro.local>
2024-12-17 17:06:28 -0500

    add post

---
+++ repo/post.ts
@@ -0,0 +1,32 @@
+import { LocaleOptions } from "@/constants";
+import { sanityClient } from "@/sanity/lib/client";
+import { SanityDocument } from "next-sanity";
+
+const POSTS_QUERY = `*[
+    _type == "blogpost" && language == $language
+    && defined(slug.current)
+  ]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt, image, tags}`;
+
+
+export const getPosts = async (language: LocaleOptions) => {
+    const posts = await sanityClient.fetch<SanityDocument[]>(
+        POSTS_QUERY,
+        {
+          language
+        },
+      );
+      return posts
+};
+
+const POST_QUERY = `*[_type == "blogpost" && slug.current == $slug && language == $locale][0]`;
+
+export const getPost = async (language: LocaleOptions, slug: string) => {
+    const post = await sanityClient.fetch<SanityDocument>(
+        POST_QUERY,
+        {
+          slug,
+          locale: language
+        },
+      );
+      return post
+};
