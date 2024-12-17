b91edc096cb33b7342f4a65222958a7f3281a92d
Arisa Shiraishi <arisashiraishi@Arisas-MacBook-Pro.local>
2024-12-17 17:23:36 -0500

    get rid of unnecesary whitespaces

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -4,10 +4,10 @@
 import { defineLive } from "next-sanity";
 import { sanityClient } from "./client";
 
-export const { sanityFetch, SanityLive } = defineLive({ 
-  client: sanityClient.withConfig({ 
+export const { sanityFetch, SanityLive } = defineLive({
+  client: sanityClient.withConfig({
     // Live content is currently only available on the experimental API
     // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX' 
-  }) 
+    apiVersion: 'vX'
+  })
 });
