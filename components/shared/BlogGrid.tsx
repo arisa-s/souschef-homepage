2a2048ed16bc263571e2636d8988250cdfd7a5a0
Arisa Shiraishi <arisashiraishi@Arisas-MacBook-Pro.local>
2024-12-24 11:14:14 -0500

    make blog page mobile friendly

--- components/shared/BlogGrid.tsx
+++ components/shared/BlogGrid.tsx
@@ -1,58 +1,71 @@
-import { getI18n } from "@/serverContexts";
-import { BlogPostCard, BlogType } from "@/types/post";
-import Image from "next/image";
-import Link from "next/link";
-import { FC } from "react";
+import { getI18n } from '@/serverContexts'
+import { BlogPostCard, BlogType } from '@/types/post'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
 
 export interface BlogGridProps {
-  posts: BlogPostCard[];
+  posts: BlogPostCard[]
 }
 
 export const BlogGrid: FC<BlogGridProps> = ({ posts }) => {
-  const totalPosts = posts.length;
-  const lastRowIndexStart = totalPosts - (totalPosts % 3 || 3);
-  const { t } = getI18n();
+  const totalPosts = posts.length
+  const lastRowIndexStart = totalPosts - (totalPosts % 3 || 3)
+  const { t } = getI18n()
 
   return (
-    <div className="px-40">
-      <div className="grid grid-cols-3">
+    <div className="sm:px-40">
+      <div className="flex flex-col divide-y sm:grid sm:grid-cols-3 sm:divide-y-0">
         {posts.map((post, index) => {
-          const isFirstColumn = index % 3 === 0;
-          const isLastRow = index >= lastRowIndexStart;
-          const borderClass = `${isFirstColumn ? "border-l" : ""} border-r ${
-            isLastRow ? "" : "border-b"
-          } border-gray-300`;
+          const isFirstColumn = index % 3 === 0
+          const isLastRow = index >= lastRowIndexStart
+          const borderClass = `${isFirstColumn ? 'sm:border-l' : ''} sm:border-r ${
+            isLastRow ? '' : 'sm:border-b'
+          }`
 
           return (
             <Link href={`/blog/${post.slug}`} key={index}>
               <div
-                className={`p-6 space-y-4 hover:bg-surface-hover cursor-pointer ${borderClass}`}
+                className={`flex cursor-pointer flex-row p-4 hover:bg-surface-hover sm:flex-col sm:space-x-0 sm:space-x-4 sm:space-y-4 sm:p-6 ${borderClass}`}
               >
-                {post.tags?.map((tag: BlogType, index: number) => (
-                  <p
-                    className="font-accent text-secondary uppercase"
-                    key={index}
-                  >
-                    {t(`${tag}Tag`)}
-                  </p>
-                ))}
-                <Image
-                  src={post.image || "/images/blog-fallback.png"}
-                  alt={`${post.title} head image`}
-                  width={500}
-                  height={500}
-                  layout="responsive"
-                />
-                <div>
-                  <label className="font-medium capitalize">{post.title}</label>
+                <div className="hidden sm:block">
+                  {post.tags?.map((tag: BlogType, index: number) => (
+                    <p className="text-secondary font-accent uppercase" key={index}>
+                      {t(`${tag}Tag`)}
+                    </p>
+                  ))}
+                </div>
+
+                <div className="mr-6 w-1/3 sm:mr-0 sm:w-auto">
+                  <Image
+                    src={post.image || '/images/blog-fallback.png'}
+                    alt={`${post.title} head image`}
+                    width={500}
+                    height={500}
+                    layout="responsive"
+                  />
+                </div>
+
+                <div className="flex flex-col justify-center">
+                  <div className="sm:hidden">
+                    {post.tags?.map((tag: BlogType, index: number) => (
+                      <p className="text-secondary font-accent uppercase" key={index}>
+                        {t(`${tag}Tag`)}
+                      </p>
+                    ))}
+                  </div>
+
+                  <label className="text-lg font-medium capitalize sm:text-base">
+                    {post.title}
+                  </label>
                 </div>
               </div>
             </Link>
-          );
+          )
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default BlogGrid;
+export default BlogGrid
