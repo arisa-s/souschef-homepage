181c13fa0c46868016fe41ba1abe29cbf25456f6
Arisa Shiraishi <arisashiraishi@Arisas-MacBook-Pro.local>
2024-12-24 11:13:51 -0500

    add animation to mobile nav

--- components/layout/MobileNavMenu.tsx
+++ components/layout/MobileNavMenu.tsx
@@ -1,30 +1,44 @@
 'use client'
 import { useState } from 'react'
-import { FiMenu } from 'react-icons/fi'
+import { FiChevronRight, FiMenu } from 'react-icons/fi'
 import LanguageChanger from '../locale/LanguageChanger'
 import Link from 'next/link'
 import { useTranslation } from 'react-i18next'
 import { emailLink, instagramProfileLink, onelinkLink } from '@/constants'
+import { motion } from 'framer-motion'
 
 export const MobileNavMenu = ({}) => {
   const [isOpen, setIsOpen] = useState(false)
   const { t } = useTranslation()
+
+  const variants = {
+    open: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+    closed: { opacity: 0, y: '-100%', transition: { duration: 0.6 } },
+  }
+
   return (
     <>
       <button onClick={() => setIsOpen(!isOpen)}>
         <FiMenu />
       </button>
       {isOpen ? (
-        <div className="absolute left-0 right-0 top-12 h-screen w-full flex-col items-center bg-surface-primary p-6">
-          <div className="divide-y">
+        <motion.div
+          initial="closed"
+          animate={isOpen ? 'open' : 'closed'}
+          variants={variants}
+          className="absolute left-0 right-0 top-12 z-20 h-screen w-full flex-col items-center bg-surface-primary p-6"
+        >
+          <div className="divide-y" onClick={() => setIsOpen(false)}>
             <MenuItem href="/blog" label={t('layout:blog')} />
             <MenuItem href={onelinkLink} label={t('home:downloadNow')} />
             <MenuItem href={emailLink} label="Email" />
             <MenuItem href={instagramProfileLink} label="Instagram" />
           </div>
-          <LanguageChanger />
+          <div className="mt-12 flex w-full justify-end">
+            <LanguageChanger />
+          </div>
           <div />
-        </div>
+        </motion.div>
       ) : null}
     </>
   )
