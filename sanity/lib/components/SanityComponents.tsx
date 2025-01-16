f24fb06eca1509ee8bcbdd9778de08ee40844c37
arisas <arisashiraishi@outlook.com>
2025-01-16 22:35:09 +0900

    hmhm

--- sanity/lib/components/SanityComponents.tsx
+++ sanity/lib/components/SanityComponents.tsx
@@ -10,16 +10,20 @@ export const SanityComponents: Partial<PortableTextReactComponents> = {
     ),
   },
   block: {
-    normal: ({ children }) => <p className="py-1 md:text-xl">{children}</p>,
-    h1: ({ children }) => <h1 className="text-3xl md:text-5xl">{children}</h1>,
-    h2: ({ children }) => <h2 className="text-2xl md:text-4xl">{children}</h2>,
-    h3: ({ children }) => <h3 className="text-xl md:text-3xl">{children}</h3>,
-    h4: ({ children }) => <h4 className="text-lg md:text-2xl">{children}</h4>,
-    h5: ({ children }) => <h5 className="md:text-xl">{children}</h5>,
-    h6: ({ children }) => <h6 className="text-sm md:text-lg">{children}</h6>,
-    list: ({ children }) => <ul className="list-disc space-y-4 pl-5 md:pl-8">{children}</ul>,
-    listItem: ({ children }) => <li className="md:text-xl">{children}</li>,
-    blockquote: ({ children }) => <blockquote className="italic">{children}</blockquote>,
+    normal: ({ children }) => (children == '' ? <br /> : <p className="md:text-xl">{children}</p>),
+    h1: ({ children }) =>
+      children == '' ? <br /> : <h1 className="text-3xl md:text-5xl">{children}</h1>,
+    h2: ({ children }) =>
+      children == '' ? <br /> : <h2 className="text-2xl md:text-4xl">{children}</h2>,
+    h3: ({ children }) =>
+      children == '' ? <br /> : <h3 className="text-xl md:text-3xl">{children}</h3>,
+    h4: ({ children }) =>
+      children == '' ? <br /> : <h4 className="text-lg md:text-2xl">{children}</h4>,
+    h5: ({ children }) => (children == '' ? <br /> : <h5 className="md:text-xl">{children}</h5>),
+    h6: ({ children }) =>
+      children == '' ? <br /> : <h6 className="text-sm md:text-lg">{children}</h6>,
+    blockquote: ({ children }) =>
+      children == '' ? <br /> : <blockquote className="italic">{children}</blockquote>,
   },
   list: {
     // Ex. 1: customizing common list types
