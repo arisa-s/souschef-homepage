1b4e402f418d371302c6a48df7ff12429374fdf0
arisas <arisashiraishi@outlook.com>
2025-01-17 19:46:47 +0900

    more experiment

--- components/layout/ImportRecipeForm.tsx
+++ components/layout/ImportRecipeForm.tsx
@@ -49,7 +49,7 @@ const ImportRecipeForm: FC = () => {
       <input
         name="query"
         placeholder={t('layout:importRecipePlaceholder')}
-        className={`appearance-none px-4 py-2 text-lg placeholder:text-sm focus:bg-surface-primary focus:outline-0 md:min-w-80`}
+        className={`w-20 appearance-none px-4 py-2 text-lg placeholder:text-sm focus:bg-surface-primary focus:outline-0 md:min-w-80`}
         value={inputValue}
         onChange={handleInputChange}
       />
