5356443bfc14bc97fee71992fb90f480145c1f67
arisas <arisashiraishi@outlook.com>
2025-04-21 10:56:35 +0900

    configure max w with inline style

--- sanity/lib/components/SanityComponents.tsx
+++ sanity/lib/components/SanityComponents.tsx
@@ -76,12 +76,7 @@ export const SanityComponents: Partial<PortableTextReactComponents> = {
     image: ({ value }) => {
       const imageUrl = getImageUrlFor(value)?.url()
       const maxWidth = value.maxWidth
-      return (
-        <img
-          src={imageUrl}
-          className={`${maxWidth ? `max-w-[${maxWidth.toString()}px] mx-auto` : ''}`}
-        />
-      )
+      return <img src={imageUrl} style={{ maxWidth: maxWidth ? `${maxWidth}px` : '100%' }} />
     },
   },
 }
